9d63f985971b1556cd5f03b2cb3111b8156d9765
Michele Mozzo <michele.mozzo@kone.com>
2024-09-26 18:16:57 +0300

    component library setup

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,19 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { ChakraProvider } from "@chakra-ui/react";
+import chakraTheme from "./styles/chakraTheme";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <ChakraProvider theme={chakraTheme}>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </ChakraProvider>
     </React.StrictMode>
 );
 
