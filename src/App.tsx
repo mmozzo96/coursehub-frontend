9f13c1a40e2b7e642537ab268e9c30b542899169
Michele Mozzo <michele.mozzo@kone.com>
2024-09-26 18:09:12 +0300

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { useGetTablesQuery } from "./app/store";
+import Router, { Switch, Route } from "crossroad";
+import LoginPage from "./pages/Login";
+import MainPage from "./pages/Main";
+import NotFound from "./pages/NotFound";
 
 function App() {
     const learnerTableQuery = useGetTablesQuery("x_quo_coursehub_learner");
@@ -11,22 +13,13 @@ function App() {
     }, [learnerTableQuery.data]);
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Router>
+            <Switch>
+                <Route path={"/"} component={LoginPage} />
+                <Route path={"/main"} component={MainPage} />
+                <Route component={NotFound} />
+            </Switch>
+        </Router>
     );
 }
 
